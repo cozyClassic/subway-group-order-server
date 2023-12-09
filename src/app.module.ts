719fc3276f400156752e69d2e466cae3c3ff5ac3
KIM <46748877+cozyClassic@users.noreply.github.com>
2023-12-09 17:29:36 +0900

    feat: 테스트용 sqlite DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MenusModule } from './menus/menus.module';
+import { User } from './users/entities/user.entity';
+import { IngredientType } from './menus/entities/handMadeFood/ingredientType.entity';
 
 @Module({
-  imports: [UsersModule, MenusModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // postgresql
+      database: 'db.sqlite',
+      autoLoadEntities: true,
+      entities: [User, IngredientType],
+      synchronize: true,
+    }),
+    UsersModule,
+    MenusModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/menus/menus.module.ts
+++ src/menus/menus.module.ts
@@ -1,9 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
 import { MenusController } from './menus.controller';
 import { IngredientTypeService } from './menus.service';
+import { IngredientType } from './entities/handMadeFood/ingredientType.entity';
+import { Menu } from './entities/menu.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Menu, IngredientType])],
   controllers: [MenusController],
   providers: [MenusService, IngredientTypeService],
 })
